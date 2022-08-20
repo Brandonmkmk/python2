c26d3d9f0bc8c305fec544c0792b55a7890b486f
Adrianxx1 <triaxis12q@gmail.com>
2022-08-20 11:03:34 -0500

    list_comprehensions

--- list and dicts.py
+++ list and dicts.py
@@ -3,9 +3,9 @@ def run():
     my_dict = {"firstname": "Brandon", "lastname": "Muñoz" }
 
 super_list = [
-     {"firstname": "Brandon", "lastname": "Muñoz" },
+     {"firstname": "Osvaldo", "lastname": "Padilla" },
      {"firstname": "Adrian", "lastname": "Martinez" },
-     {"firstname": "Cristian", "lastname": "Ramos" },
+     {"firstname": "Alma", "lastname": "Ramos" },
      {"firstname": "Paco", "lastname": "Sanchez" },
      {"firstname": "Sonia", "lastname": "Muñoz" },
 ]
