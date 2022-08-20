6e3d33b20b964ad0799516ed3de03d680e47e5d9
Adrianxx1 <triaxis12q@gmail.com>
2022-08-20 10:32:53 -0500

    tercer commit

---
+++ list and dicts.py
@@ -0,0 +1,24 @@
+def run():
+my_list = [1, "Hello", True, 4.5]
+my_dict = {"firstname": "Brandon", "lastname": "Muñoz" }
+
+super_list = [
+     {"firstname": "Brandon", "lastname": "Muñoz" },
+     {"firstname": "Adrian", "lastname": "Martinez" },
+     {"firstname": "Cristian", "lastname": "Ramos" },
+     {"firstname": "Paco", "lastname": "Sanchez" },
+     {"firstname": "Sonia", "lastname": "Muñoz" },
+]
+
+super_dict = {
+    "natural_nums":[1, 2, 3, 4, 5],
+    "integrer_nums":[-1, -2, 0, 1, 2],
+    "floating_nums":[1.1, 4.5, 6.43],
+}
+
+for key, value in super_dict.items():
+    print(key, "-", value)
+
+
+if __name__ == '__main__':
+run() 
